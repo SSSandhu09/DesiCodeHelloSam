11222961df5d967e904e2c17e88768f2dd32d9c2
SSSandhu09 <143095851+SSSandhu09@users.noreply.github.com>
2024-02-05 16:02:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 class human:
   def detailsInput(self):
-    name = input("Enter your name:");
-    age = input("Enter your age:");
+    self.name = input("Enter your name:");
+    selfage = input("Enter your age:");
 
   def printDetails(self):
-    print(f"Name:{self.name} \nAge:{self.name}")
+    print(f"Name:{self.name} \nAge:{self.age}")
 
 samnit = human()
 samnit.detailsInput()
